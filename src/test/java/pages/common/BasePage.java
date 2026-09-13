b26110adac83b9e01487c8ace2a5edb6c40b2f44
Ricardo8421 <jricardogomeztagle@gmail.com>
2026-09-13 15:53:27 -0600

    Import issues

--- src/test/java/pages/common/BasePage.java
+++ src/test/java/pages/common/BasePage.java
@@ -1,7 +1,5 @@
 package pages.common;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
